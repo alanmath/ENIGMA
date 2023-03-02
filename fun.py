54a75415f3449cd96b86b84826e48d625f4a2015
esdrasgc <esdrasgcdev@gmail.com>
2023-03-02 18:02:45 -0300

    comentários docString

--- fun.py
+++ fun.py
@@ -4,6 +4,10 @@ import random
 alphabet = "abcdefghijklmnopqrstuvwxyz "
 
 def para_one_hot(msg: str):
+    """
+    Função que converte uma string em uma matriz one-hot,
+    onde cada linha representa um caracter do alfabeto e cada coluna representa um caracter da mensagem.
+    """
     char_to_index = {}
 
     for i, char in enumerate(alphabet):
@@ -19,6 +23,9 @@ def para_one_hot(msg: str):
 
 
 def para_string(M: np.array):
+    """
+    Função que converte uma matriz one-hot em uma string.
+    """
     msg = ""
     for i in range(M.shape[1]):   # iterar através das colunas (caracteres) da matriz
         indices = np.where(M[:,i] == 1)[0]  # encontra os índices dos elementos igual a 1 na coluna atual
@@ -30,6 +37,13 @@ def para_string(M: np.array):
 
 
 def cifrar(msg:str, P:np.array):
+    """
+    Função que aplica uma cifra de substituição simples a uma mensagem,
+    consiste em substituir cada caracter da mensagem por um caracter de um alfabeto cifrador.
+    Essa substituição é feita trocando o elemento n do alfabeto cifrador pelo elemento n do alfabeto original.
+    Essa operação é feita multiplicando a matriz P (matriz permutação) pelo vetor M, onde P é a matriz one-hot do alfabeto cifrador,
+    e M é a matriz one-hot da mensagem.
+    """
     M = para_one_hot(msg)
     C = np.dot(P, M)
 
@@ -37,13 +51,27 @@ def cifrar(msg:str, P:np.array):
     
 
 def de_cifrar(msg: str, P:np.array):
+    """
+    Função que decifra uma mensagem cifrada com a função cifrar.
+    Ou seja, recupera a mensagem original a partir da mensagem cifrada e da matriz P.
+    """
     P_INV = np.linalg.inv(P)
     C = para_one_hot(msg)
     M_DECIFRADA = np.dot(P_INV, C)
     msg = para_string(M_DECIFRADA)
     return msg
 
+
+
 def enigma(msg:str, seed:int = 40):
+    """
+    Função que aplica uma cifra semelhante à cifra da máquina Enigma,
+    famosa máquina de criptografia usada pelos nazistas durante a Segunda Guerra Mundial.
+    Consiste em aplicar uma cifra de substituição simples a cada caracter da mensagem,
+    onde cada caracter da mensagem é substituído por um caracter de um alfabeto cifrador
+    que é cifrado por um alfabeto auxiliar, a cada substituição.
+
+    """
     random.seed(seed)
     alfabeto_cifrador = list(alphabet)
     random.shuffle(list(alfabeto_cifrador))
@@ -52,7 +80,7 @@ def enigma(msg:str, seed:int = 40):
     cifrador_aux = list(alfabeto_cifrador)
     random.shuffle(cifrador_aux)
     cifrador_aux = "".join(cifrador_aux)
-    
+
     M = para_one_hot(msg)
     P = para_one_hot(alfabeto_cifrador)
     E = para_one_hot(cifrador_aux)
@@ -66,6 +94,9 @@ def enigma(msg:str, seed:int = 40):
 
 
 def de_enigma(msg:str, seed:int = 40):
+    """
+    Função que decifra uma mensagem cifrada com a função enigma.
+    """
     random.seed(seed)
     alfabeto_cifrador = list(alphabet)
     random.shuffle(list(alfabeto_cifrador))
